86bb786cfeabf3e4964494d78d0be70c0ca02166
Behram-Khattak <behramkttk9@gmail.com>
2024-05-08 23:28:37 +0500

    Forced the app to run the vite on https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        URL::forceScheme('https');
     }
 
     /**
