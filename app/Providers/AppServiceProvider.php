264976a7e4d019276900e28b344c8b972529f4ce
Behram-Khattak <behramkttk9@gmail.com>
2024-10-24 22:11:20 +0500

    Added carbon time now() method to change year autimaticaly

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        URL::forceScheme('https');
+        // URL::forceScheme('https');
     }
 
     /**
--- resources/views/components/frontend-footer.blade.php
+++ resources/views/components/frontend-footer.blade.php
@@ -1,3 +1,3 @@
-<div class="w-full text-center p-4 border-t bg-gray-50">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="{{ route('frontend.home') }}" class="hover:underline">Blog</a>. All Rights Reserved.</span>
+<div class="w-full p-4 text-center border-t bg-gray-50">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ now()->format('Y') }} <a href="{{ route('frontend.home') }}" class="hover:underline">Blog</a>. All Rights Reserved.</span>
 </div>
